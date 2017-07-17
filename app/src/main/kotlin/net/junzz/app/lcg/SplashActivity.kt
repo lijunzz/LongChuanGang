412b4c84126087c38de6b938be6eaab983606aa4
Junzz <lijunzz@users.noreply.github.com>
2017-07-17 23:38:57 +0800

    添加 Kotlin

---
+++ app/src/main/kotlin/net/junzz/app/lcg/SplashActivity.kt
@@ -0,0 +1,11 @@
+package net.junzz.app.lcg
+
+import android.app.Activity
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
